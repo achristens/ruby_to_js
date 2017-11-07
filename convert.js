934413961dd7101415d4432e8d6544fcde32f2e7
Abby Christens <achristens@gmail.com>
2017-11-07 12:42:31 -0500

    Convert Hash to object literal

--- convert.js
+++ convert.js
@@ -1,3 +1,13 @@
 var apples = 14;
 console.log(apples);
 console.log("I have " + apples + " apples.");
+
+console.log("===========================");
+
+var materials = ['wood', 'metal', 'stone']
+
+var words = {
+  "elephant": "The world's largest land mammal.",
+  "school": "A place of learning.",
+  "ice cream": "A delicious milk-based dessert."
+}
--- original.rb
+++ original.rb
@@ -2,3 +2,12 @@ apples = 14
 puts apples
 
 puts "I have #{apples} apples."
+
+puts "==========================="
+
+materials = ['wood', 'metal', 'stone']
+words = {
+  'elephant' => "The world's largest land mammal.",
+  'school' => 'A place of learning.',
+  'ice cream' => 'A delicious milk-based dessert.',
+}
